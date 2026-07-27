737fe1b4f977824594c50ba564e4967472cb74e0
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-07-27 12:07:01 +0100

    ..

--- crop-predict/src/components/Fallback.tsx
+++ crop-predict/src/components/Fallback.tsx
@@ -8,16 +8,25 @@ type FallbackProviderProps = {
 
 const Fallback = ({ message, mode }: FallbackProviderProps) => {
 	const ctx = useContext(DataContext);
+	let text;
+	if (mode == "default") {
+		if (ctx?.language == "English") {
+			text = message;
+		} else {
+			text =
+				"Labari mai dadi — wannan amfanin gona yana da kyau sosai! Ba ya bukatar wani magani.";
+		}
+	} else {
+		if (ctx?.language == "English") {
+			text = message;
+		} else {
+			text = "Babu sakamako. Da fatan za a sake gwadawa.";
+		}
+	}
 	return (
 		<div className="grow w-full h-30 flex justify-center items-center shrink-0">
 			<div className=" h-fit flex flex-col gap-.5  p-3 px-4 rounded-lg border-2 border-[#A8D0C5] bg-[#F7FAF9] w-full">
-				<span className="text-[#0F6E56] text-md font-bold">
-					{mode == "default"
-						? message
-						: ctx?.language == "English"
-							? "Congratulations! You have a healthy plant!"
-							: "Taya murna! Kana da shuka mai lafiya!"}
-				</span>
+				<span className="text-[#0F6E56] text-md font-bold">{text}</span>
 			</div>
 		</div>
 	);
