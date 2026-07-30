b9e7db998b940d86480f68c9bef9289db2f85f56
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-07-30 11:21:27 +0100

    Edited hausa translation for loading screen

--- crop-predict/src/pages/LoadingScreen.tsx
+++ crop-predict/src/pages/LoadingScreen.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import LoadingAnimation from "../components/LoadingAnimation";
+import DataContext from "../context/DataContext";
 
 const LoadingScreen = () => {
+	const ctx = useContext(DataContext);
 	return (
 		<div className="w-screen h-screen bg-[#1f1e1e81] flex justify-center items-center fixed top-0 left-0 overflow-hidden z-50">
 			<article className="bg-white p-5 rounded-xl flex flex-col gap-4 items-center justify-center border-2 border-[#E8F5F1] ">
 				<LoadingAnimation />
 				<span className="text-sm font-semibold">
-					Analyzing your photo...
+					{ctx?.language == "English"
+						? "Analyzing your photo..."
+						: "Ana nazarin hotonka..."}
 				</span>
 			</article>
 		</div>
